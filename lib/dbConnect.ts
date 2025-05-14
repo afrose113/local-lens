1eb481ce1292186bf4ba89cbac125562d652f3a5
afrose113 <afroseali7032@gmail.com>
2025-05-14 14:32:06 -0500

    Fix globalThis MongoClient typing for TypeScript

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,28 +1,20 @@
-// lib/dbConnect.ts
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI || "mongodb://localhost:27017";
+const uri = process.env.MONGODB_URI!;
 const options = {};
 
 let client;
 let clientPromise: Promise<MongoClient>;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local');
+declare global {
+  // Allow adding custom properties to globalThis
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-if (process.env.NODE_ENV === 'development') {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
+if (!global._mongoClientPromise) {
   client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  global._mongoClientPromise = client.connect();
 }
+clientPromise = global._mongoClientPromise;
 
-export async function dbConnect() {
-  const client = await clientPromise;
-  return client.db("localnews");
-}
+export default clientPromise;
